1d4399f0d2cb8bb610dbe645164718b98b5853f0
karanOO89 <gk.987680@gmail.com>
2021-05-29 02:01:26 +0000

    added categories

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,6 @@
 class ProductsController < ApplicationController
   
-  http_basic_authenticate_with name: ENV['username'], password: ENV['password']
+  # http_basic_authenticate_with name: ENV['username'], password: ENV['password']
 
 
   def index
@@ -12,14 +12,3 @@ class ProductsController < ApplicationController
   end
 
 end
-
-# class PostsController < ApplicationController
-
-#   def index
-#     render plain: "Everyone can see me!"
-#   end
-
-#   def edit
-#     render plain: "I'm only accessible if you know the password"
-#   end
-# end
