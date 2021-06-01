169e4a1f0a65e6a947f545ed2a583fc0b0aedc7b
karanOO89 <gk.987680@gmail.com>
2021-06-01 23:44:09 +0000

    cart feature test completed

---
+++ spec/features/add_to_cart_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.feature "AddToCarts", type: :feature do
+  # pending "add some scenarios (or delete) #{__FILE__}"
+  before :each do
+    @category = Category.create! name: 'Apparel'
+  
+    10.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+  scenario "They see cart updations on clicking Add button" do
+    visit root_path
+
+    expect(page).to have_text('My Cart (0)')
+    page.save_screenshot("2.png", :full => true)
+    click_button('Add',match: :first)
+    expect(page).to have_text('My Cart (1)')
+    save_screenshot("3.png", :full => true)
+
+  end
+end
