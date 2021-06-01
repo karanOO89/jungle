0875b509f45b0757dd0549a124e22bd8da6b88be
karanOO89 <gk.987680@gmail.com>
2021-06-01 04:06:07 +0000

    user validations completed with rspec

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,28 @@
 class User < ActiveRecord::Base
     has_secure_password
+    
+
+    validates :password, presence: true
+    validates :email, presence: true, uniqueness: true
+    validates :first_name, presence: true
+    validates :last_name, presence: true
+    validates :password, length: {minimum: 5, maximum: 14 }
+    
+    before_save :prep_email
+
+   def prep_email
+      self.email.downcase!
+      self.email.gsub!(/ /,"")
+   end
+  
+    def self.authenticate_with_credentials(email,password)
+        user = User.find_by_email(email)
+
+            if user && user.authenticate(password)
+                user
+            else
+                nil
+            end
+    end    
+    # If the user exists AND the password entered is correct.
 end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,85 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+
+  describe "Validations" do
+    it "is valid with valid attributes" do
+      user = User.new
+      user.email ="gk.987680@gmail.com"
+      user.password="123456"
+      user.password_confirmation="1234556"
+      user.first_name = "karan"
+      user.last_name = "gaba"
+    expect(user).to_not be_valid
+    expect(user.errors.full_messages).to include("Password confirmation doesn't match Password")
+    end
+
+    it "email existence error" do
+      user = User.create({
+        first_name: "karan",
+        last_name:"gaba", 
+        email: "gk.987680@gmail.com", 
+        password: "12345", 
+        password_confirmation: "12345"})
+      user2 = User.create({
+        first_name: "karan",
+        last_name:"gaba", 
+        email: "gk.987680@gmail.com", 
+        password: "12345", 
+        password_confirmation: "12345"})
+      expect(user2).to_not be_valid
+      expect(user2.errors.full_messages).to include("Email has already been taken")
+    end
+
+    it "min password length > 5 chars " do
+      user = User.create({
+        first_name: "karan",
+        last_name:"gaba", 
+        email: "gsk.987680@gmail.com", 
+        password: "125", 
+        password_confirmation: "125"})
+        expect(user).to_not be_valid
+        expect(user.errors.full_messages).to include("Password is too short (minimum is 5 characters)")
+    end
+
+    it "min password length < 14 chars " do
+      user = User.create({
+        first_name: "karan",
+        last_name:"gaba", 
+        email: "gsak.987680@gmail.com", 
+        password: "123456789101112", 
+        password_confirmation: "123456789101112"})
+        expect(user).to_not be_valid
+        expect(user.errors.full_messages).to include("Password is too long (maximum is 14 characters)")
+    end
+  end
+
+
+  describe '.authenticate_with_credentials' do
+    it "should fail if invalid email is used to login" do
+      user = User.create({
+        first_name: "karan",
+        last_name:"gaba", 
+        email: "gk.987680@gmail.com", 
+        password: "12345", 
+        password_confirmation: "12345"})
+
+      # user1 = User.authenticate_with_credentials("gk.987680@gmail.com","12345")
+      expect(User.authenticate_with_credentials("gk.987680@gmail.com","12345")).to be
+
+    end
+    it "should pass even if email with uppercase and space is used for login and registration" do
+      user = User.create({
+        first_name: "karan",
+        last_name:"gaba", 
+        email: "   gk  .987680 @GMAil.com", 
+        password: "12345", 
+        password_confirmation: "12345"})
+
+      # user1 = User.authenticate_with_credentials("gk.987680@gmail.com","12345")
+      expect(User.authenticate_with_credentials("gk.987680@gmail.com","12345")).to be
+      expect(user).to be_valid  
+      
+    end
+  end
+end
